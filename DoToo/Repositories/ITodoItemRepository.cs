9f6ef855bd919da145b9ad42f1cad60ee47fd2c6
kehinde <kehinde@learnflo.co>
2025-06-06 11:01:42 +0100

    Add project files.

---
+++ DoToo/Repositories/ITodoItemRepository.cs
@@ -0,0 +1,15 @@
+﻿using DoToo.Models;
+
+namespace DoToo.Repositories
+{
+    public interface ITodoItemRepository
+    {
+        event EventHandler<TodoItem> OnItemAdded;
+        event EventHandler<TodoItem> OnItemUpdated;
+
+        Task<List<TodoItem>> GetItemsAsync();
+        Task AddItemAsync(TodoItem item);
+        Task UpdateItemAsync(TodoItem item);
+        Task AddOrUpdateAsync(TodoItem item);
+    }
+}
