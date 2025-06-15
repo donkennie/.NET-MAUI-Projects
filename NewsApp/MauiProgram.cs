bbf7eb699299ca74486f34eb1a3e046475938932
donkennie <ajeigbekehinde160@gmail.com>
2025-06-15 18:51:50 +0100

    add flyout and shell navigation

--- NewsApp/MauiProgram.cs
+++ NewsApp/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Hosting;
 
 namespace NewsApp
 {
@@ -11,6 +12,7 @@ namespace NewsApp
                 .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
                 {
+                    fonts.AddFont("FontAwesome.otf", "FontAwesome");
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 })
