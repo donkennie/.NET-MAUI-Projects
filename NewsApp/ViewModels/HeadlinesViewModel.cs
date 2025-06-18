f268b40c212a51e5c22b44e04ddfaeffa392cd7b
donkennie <ajeigbekehinde160@gmail.com>
2025-06-18 20:11:54 +0100

    add headline view model logic

--- NewsApp/ViewModels/HeadlinesViewModel.cs
+++ NewsApp/ViewModels/HeadlinesViewModel.cs
@@ -1,12 +1,44 @@
-﻿using News.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using News.ViewModels;
+using NewsApp.Models;
+using NewsApp.Services;
+using System.Web;
 
 namespace NewsApp.ViewModels
 {
     public partial class HeadlinesViewModel : ViewModel
     {
-        public HeadlinesViewModel()
+        private readonly INewsService newsService;
+
+        [ObservableProperty]
+        private NewsResult CurrentNews;
+
+        public HeadlinesViewModel(INewsService newsService)//, INavigate navigation) : base(navigation)
         {
-            
+            this.newsService = newsService;
+        }
+
+        public async Task Initialize(string scope) => await Initialize(scope.ToLower() switch
+        {
+            "local" => NewsScope.Local,
+            "global" => NewsScope.Global,
+            "headlines" => NewsScope.Headlines,
+            _ => NewsScope.Headlines
+        });
+
+        public async Task Initialize(NewsScope scope)
+        {
+            CurrentNews = await newsService.GetNews(scope);
+        }
+
+        [RelayCommand]
+        public async Task ItemSelected(object selectedItem)
+        {
+            var selectedArticle = selectedItem as Article;
+            var url = HttpUtility.UrlEncode(selectedArticle.Url);
+           // await Navigation.NavigateTo($"articleview?url={url}");
         }
     }
+    
 }
--- NewsApp/Views/HeadlinesView.xaml.cs
+++ NewsApp/Views/HeadlinesView.xaml.cs
@@ -1,9 +1,29 @@
+using NewsApp.ViewModels;
+
 namespace NewsApp.Views;
 
 public partial class HeadlinesView : ContentPage
 {
-	public HeadlinesView()
+
+	readonly HeadlinesViewModel viewModel;
+
+	public HeadlinesView(HeadlinesViewModel viewModel)
 	{
+		this.viewModel = viewModel;
 		InitializeComponent();
+
+		Task.Run(async () => await Initialize(GetScopeFromRoute()));
+	}
+
+	private async Task Initialize(string scope)
+	{
+		BindingContext = viewModel;
+		await viewModel.Initialize(scope);
+	}
+
+	private string GetScopeFromRoute()
+	{
+		var route = Shell.Current.CurrentState.Location.OriginalString.Split("/").LastOrDefault();
+		return route;
 	}
 }
